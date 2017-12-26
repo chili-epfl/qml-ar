2e0f16cf6726d57287c37c146e94a5b0c89c7882
Sergei Volodin <etoestja@yandex.ru>
2017-12-27 00:59:35 +0100

    Fix image loading

--- src/imagebackend/imagebackend.cpp
+++ src/imagebackend/imagebackend.cpp
@@ -1,8 +1,11 @@
 #include "imagebackend.h"
+#include <QDebug>
 
 ImageBackend::ImageBackend(QString filename) : QQuickImageProvider(QQuickImageProvider::Pixmap)
 {
-    Q_ASSERT(buffer.load(filename));
+    QImage img(filename);
+    Q_ASSERT(!img.isNull());
+    buffer = img;
 }
 
 QPixmap ImageBackend::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize)
