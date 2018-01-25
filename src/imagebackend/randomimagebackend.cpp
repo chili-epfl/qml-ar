bf18f7b710c3a46b5afd76b24ec2b9c9569a2bed
Sergei Volodin <etoestja@yandex.ru>
2018-01-25 14:24:14 +0100

    Add random image backend

--- src/imagebackend/randomimagebackend.cpp
+++ src/imagebackend/randomimagebackend.cpp
@@ -1,6 +1,19 @@
 #include "randomimagebackend.h"
 
-RandomImageBackend::RandomImageBackend()
+RandomImageBackend::RandomImageBackend(int w, int h, int dt_ms) : ImageProviderAsync()
 {
+    this->w = w;
+    this->h = h;
+    data = (uchar*) malloc(w * h * 3 * max_images);
+    connect(&t, &QTimer::timeout, this, &RandomImageBackend::updateImage);
+    t.start(dt_ms);
+    current_image = 0;
+}
 
+void RandomImageBackend::updateImage()
+{
+    QImage image = QImage(data + current_image * w * h * 3, w, h, QImage::Format_RGB888);
+    current_image++;
+    current_image %= max_images;
+    emit imageAvailable(image.copy());
 }
--- src/imagebackend/randomimagebackend.h
+++ src/imagebackend/randomimagebackend.h
@@ -1,11 +1,40 @@
 #ifndef RANDOMIMAGEBACKEND_H
 #define RANDOMIMAGEBACKEND_H
 
+#include "imageproviderasync.h"
+#include <QImage>
+#include <QTimer>
 
-class RandomImageBackend
-{
+/*
+ * This class takes input from memory
+ * and outputs it as a QImage
+ */
+
+class RandomImageBackend : public ImageProviderAsync
+{ Q_OBJECT
+private:
+    // pointer to data
+    uchar* data;
+
+    // timer to update
+    QTimer t;
+
+    // maximum number of images
+    const int max_images = 100;
+
+    // width and height parameters
+    int w, h;
+
+    // image to return
+    int current_image;
+
 public:
-    RandomImageBackend();
+    RandomImageBackend(int w = 640, int h = 480, int dt_ms = 10);
+
+public slots:
+
+    // generate and emit new image
+    void updateImage();
 };
 
 #endif // RANDOMIMAGEBACKEND_H
