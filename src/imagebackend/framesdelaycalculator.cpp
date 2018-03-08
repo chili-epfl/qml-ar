d06e789331f24375bd8d4a4fafd427862347d604
Sergei Volodin <etoestja@yandex.ru>
2018-03-08 17:39:01 +0100

    Implement FramesDelayCalculator

--- src/imagebackend/framesdelaycalculator.cpp
+++ src/imagebackend/framesdelaycalculator.cpp
@@ -2,5 +2,38 @@
 
 FramesDelayCalculator::FramesDelayCalculator()
 {
+    // some initial values
+    mean_latency = 0;
+    mean_fps = 30;
+    result = 0;
+}
 
+int FramesDelayCalculator::lastDelay()
+{
+    return result;
+}
+
+int FramesDelayCalculator::getDelay()
+{
+    // rewinding back to proper timing
+    int delay_in_frames = qRound(mean_latency * mean_fps / 1000.);
+    return delay_in_frames;
+}
+
+void FramesDelayCalculator::update()
+{
+    result = getDelay();
+    emit newDelay(result);
+}
+
+void FramesDelayCalculator::setMeanLatency(double lat_ms)
+{
+    mean_latency = lat_ms;
+    update();
+}
+
+void FramesDelayCalculator::setMeanFPS(double fps_ms)
+{
+    mean_fps = fps_ms;
+    update();
 }
--- src/imagebackend/framesdelaycalculator.h
+++ src/imagebackend/framesdelaycalculator.h
@@ -1,11 +1,41 @@
 #ifndef FRAMESDELAYCALCULATOR_H
 #define FRAMESDELAYCALCULATOR_H
 
+/*
+ * This class calculates the delay in frames based on desired latency and
+ * actual FPS
+ */
 
-class FramesDelayCalculator
-{
+#include <QObject>
+
+class FramesDelayCalculator : public QObject
+{ Q_OBJECT
 public:
     FramesDelayCalculator();
+
+    // returns last value
+    int lastDelay();
+
+public slots:
+    void setMeanLatency(double lat_ms);
+    void setMeanFPS(double fps_ms);
+
+private:
+    // mean latency from camera to markers
+    double mean_latency;
+    double mean_fps;
+
+    // result in frames
+    int result;
+
+    // update local result
+    void update();
+
+    // returns delay in frames
+    int getDelay();
+
+signals:
+    void newDelay(int);
 };
 
 #endif // FRAMESDELAYCALCULATOR_H
